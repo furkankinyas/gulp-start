afc8117b848396186a2bf1382c1cfb63997e39e3
furkan kinyas <kinyasfurkan@gmail.com>
2021-03-08 11:37:13 +0300

    amp

--- gulpfile.js
+++ gulpfile.js
@@ -1,188 +1,237 @@
-const  { src, dest, parallel, watch, series } = require('gulp'),
-        sass = require('gulp-sass'),
-        cssmin = require('gulp-clean-css'),
-        concat = require('gulp-concat'),
-        uglify = require('gulp-uglify'),
-        browserSync = require('browser-sync'),
-        reload = browserSync.reload,
-        size = require('gulp-size'),
-        imagemin = require('gulp-imagemin'),
-        imageminOptipng = require('imagemin-optipng'),
-        imageminSvgo = require('imagemin-svgo'),
-        imageminMozjpeg = require('imagemin-mozjpeg'),
-        plumber = require('gulp-plumber'),
-        notify = require('gulp-notify'),
-        pug = require('gulp-pug'),
-        iconfont = require('gulp-iconfont'),
-        iconfontCss = require('gulp-iconfont-css'),
-        spritesmith = require('gulp.spritesmith'),
-        buffer = require('vinyl-buffer'),
-        merge = require('merge-stream');
+const { src, dest, parallel, watch, series } = require('gulp'),
+  sass = require('gulp-sass'),
+  cssmin = require('gulp-clean-css'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  browserSync = require('browser-sync'),
+  reload = browserSync.reload,
+  size = require('gulp-size'),
+  imagemin = require('gulp-imagemin'),
+  imageminOptipng = require('imagemin-optipng'),
+  imageminSvgo = require('imagemin-svgo'),
+  imageminMozjpeg = require('imagemin-mozjpeg'),
+  plumber = require('gulp-plumber'),
+  notify = require('gulp-notify'),
+  pug = require('gulp-pug'),
+  iconfont = require('gulp-iconfont'),
+  iconfontCss = require('gulp-iconfont-css'),
+  spritesmith = require('gulp.spritesmith'),
+  buffer = require('vinyl-buffer'),
+  merge = require('merge-stream'),
+  purgecss = require("gulp-purgecss"),
+  replace = require('gulp-replace');
 
-// npm install gulp gulp-sass gulp-clean-css gulp-concat gulp-uglify browser-sync gulp-size gulp-imagemin imagemin-optipng imagemin-svgo imagemin-mozjpeg gulp-plumber gulp-notify gulp-pug gulp-iconfont gulp-iconfont-css gulp.spritesmith vinyl-buffer merge-stream --save-dev
+// npm install gulp gulp-sass gulp-clean-css gulp-concat gulp-uglify browser-sync gulp-size gulp-imagemin imagemin-optipng imagemin-svgo imagemin-mozjpeg gulp-plumber gulp-notify gulp-pug gulp-iconfont gulp-iconfont-css gulp.spritesmith vinyl-buffer merge-stream gulp-purgecss gulp-replace --save-dev
 
 
 
 // pug dosylarını html çevirir
-function pughtml(){
-    return src('src/views/**/*.pug')
-        .pipe(pug({
-            pretty: true
-        }))
-        .pipe(dest('dist/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+function pughtml() {
+  return src(['src/views/**/*.pug', 'src/views-amp/**/*.pug'])
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(dest('dist/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 }
 // sprite oluşturur
-function sprite(){
-    var spriteData = src('src/img/sprite/*.png')
-        .pipe(spritesmith({
-            imgName: 'sprite.png',
-            cssName: 'sprite.css',
-            imgPath: '../../img/sprite.png?v='+Date.now(),
-            retinaImgPath: '../../img/sprite@2x.png?v='+Date.now(),
-            retinaSrcFilter: 'src/img/sprite/*@2x.png',
-            retinaImgName: 'sprite@2x.png',
-            padding: 5
-        }));
-    var imgStream = spriteData.img
-        .pipe(buffer())
-        .pipe(imagemin([imagemin.optipng()]))
-        .pipe(dest('dist/assets/img'));
-    var cssStream = spriteData.css
-        .pipe(dest('src/scss/gulp/'))
-    return merge(imgStream, cssStream);
+function sprite() {
+  var spriteData = src('src/img/sprite/*.png')
+    .pipe(spritesmith({
+      imgName: 'sprite.png',
+      cssName: 'sprite.css',
+      imgPath: '../../img/sprite.png?v=' + Date.now(),
+      retinaImgPath: '../../img/sprite@2x.png?v=' + Date.now(),
+      retinaSrcFilter: 'src/img/sprite/*@2x.png',
+      retinaImgName: 'sprite@2x.png',
+      padding: 5
+    }));
+  var imgStream = spriteData.img
+    .pipe(buffer())
+    .pipe(imagemin([imagemin.optipng()]))
+    .pipe(dest('dist/assets/img'));
+  var cssStream = spriteData.css
+    .pipe(dest('src/scss/gulp/'))
+  return merge(imgStream, cssStream);
 }
 // font dosyası oluşturur
-function icon(){
-    return src('src/icon/*.svg')
-        .pipe(iconfontCss({
-            fontName: 'icon',
-            // template path
-            path: 'src/scss/gulp/_icon-template.scss',
-            // will generate scss
-            targetPath: '../../../src/scss/gulp/_icon.scss',
-            // template font path
-            fontPath: '../font/'
-        }))
-        .pipe(iconfont({
-            fontName: 'icon',
-            formats: ['ttf', 'eot', 'woff', 'woff2'],
-            normalize: true,
-            fontHeight: 2000
-        }))
-        // will generate font
-        .pipe(dest('dist/assets/font/'));
+function icon() {
+  return src('src/icon/*.svg')
+    .pipe(iconfontCss({
+      fontName: 'icon',
+      // template path
+      path: 'src/scss/gulp/_icon-template.scss',
+      // will generate scss
+      targetPath: '../../../src/scss/gulp/_icon.scss',
+      // template font path
+      fontPath: '../font/'
+    }))
+    .pipe(iconfont({
+      fontName: 'icon',
+      formats: ['ttf', 'eot', 'woff', 'woff2'],
+      normalize: true,
+      fontHeight: 2000
+    }))
+    // will generate font
+    .pipe(dest('dist/assets/font/'));
 }
 // sass css çevirir
-function scss(){
-    var onError = function (err) {
-        notify.onError({
-            title: "Gulp",
-            subtitle: "Failure!",
-            message: "Error: <%= error.message %>",
-            sound: "Beep"
-        })(err);
-        this.emit('end');
-    };
+function scss() {
+  var onError = function (err) {
+    notify.onError({
+      title: "Gulp",
+      subtitle: "Failure!",
+      message: "Error: <%= error.message %>",
+      sound: "Beep"
+    })(err);
+    this.emit('end');
+  };
 
-    return src('src/scss/**/*.scss')
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(sass())
-        .pipe(cssmin())
-        .pipe(size({
-            gzip: true,
-            showFiles: true
-        }))
-        .pipe(dest('dist/assets/css/'))
-        .pipe(browserSync.reload({
-          stream: true
-        }))
+  return src('src/scss/**/*.scss')
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(sass())
+    .pipe(purgecss({
+      content: ['src/views/**/*.pug', 'src/js/*.js']
+    }))
+    .pipe(cssmin())
+    .pipe(size({
+      gzip: true,
+      showFiles: true
+    }))
+    .pipe(dest('dist/assets/css/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+}
+// amp sass css çevirir
+function scssAmp() {
+  var onError = function (err) {
+    notify.onError({
+      title: "Gulp",
+      subtitle: "Failure!",
+      message: "Error: <%= error.message %>",
+      sound: "Beep",
+    })(err);
+    this.emit("end");
+  };
+
+  return (
+    src("src/scss/amp.scss")
+      .pipe(
+        plumber({
+          errorHandler: onError,
+        })
+      )
+      .pipe(sass())
+      .pipe(purgecss({
+        content: ['src/amp-views/*/**.pug']
+      }))
+      .pipe(cssmin())
+      .pipe(replace(/@charset "UTF-8";/g, ""))
+      // .pipe(replace(/..\/img\//g, "https://assets.turkmedya.com.tr/platin/assets/img/"))
+      // .pipe(replace(/..\/font\//g, "https://assets.turkmedya.com.tr/platin/assets/font/"))
+      .pipe(replace(/!important/g, ""))
+      .pipe(
+        size({
+          gzip: true,
+          showFiles: true,
+        })
+      )
+      .pipe(dest("dist/assets/css/"))
+      .pipe(
+        browserSync.reload({
+          stream: true,
+        })
+      )
+  );
 }
 // browser refresh eder
 function browser() {
-    browserSync({
-        server: {
-            baseDir: "./dist"
-        }
-    });
+  browserSync({
+    server: {
+      baseDir: "./dist"
+    }
+  });
 }
 // js min ve birleştirme
-function js(){
-    return src(['src/js/plugin.js', 'src/js/scripts.js'])
-        .pipe(concat('scripts-min.js'))
-        .pipe(uglify())
-        .pipe(size({
-            gzip: true,
-            showFiles: true
-        }))
-        .pipe(dest('dist/assets/js/min'))
-        .pipe(reload({
-            stream: true
-        }));
+function js() {
+  return src(['src/js/plugin.js', 'src/js/scripts.js'])
+    .pipe(concat('scripts-min.js'))
+    .pipe(uglify())
+    .pipe(size({
+      gzip: true,
+      showFiles: true
+    }))
+    .pipe(dest('dist/assets/js/min'))
+    .pipe(reload({
+      stream: true
+    }));
 }
 // jsleri tek tek kullanım için minify etme
-function singlejs(){
+function singlejs() {
   return src('src/js/single/*.js')
-      .pipe(uglify())
-      .pipe(size({
-          gzip: true,
-          showFiles: true
-      }))
-      .pipe(dest('dist/assets/js/min'))
+    .pipe(uglify())
+    .pipe(size({
+      gzip: true,
+      showFiles: true
+    }))
+    .pipe(dest('dist/assets/js/min'))
 }
 // plugin js min ve birleştirme
-function pluginjs(){
-    return src('src/js/plugin/*.js')
-        .pipe(concat('plugin.js'))
-        .pipe(dest('src/js'))
+function pluginjs() {
+  return src('src/js/plugin/*.js')
+    .pipe(concat('plugin.js'))
+    .pipe(dest('src/js'))
 }
 // src font dosyalarını dist taşır
-function font(){
-    return src('src/font/*')
-        .pipe(dest('dist/assets/font/'))
+function font() {
+  return src('src/font/*')
+    .pipe(dest('dist/assets/font/'))
 }
 // resimleri min etme
-function imgmin(){
-    return src(['src/img/*'])
-        .pipe(buffer())
-        .pipe(imagemin(
-            [
-                imageminMozjpeg({
-                    quality: 90
-                }),
-                imageminOptipng(),
-                imageminSvgo({
-                    plugins: [{
-                        removeViewBox: false
-                    }]
-                })
-            ]))
-        .pipe(dest('dist/assets/img'))
-        .pipe(reload({
-            stream: true
-        }));
+function imgmin() {
+  return src(['src/img/*'])
+    .pipe(buffer())
+    .pipe(imagemin(
+      [
+        imageminMozjpeg({
+          quality: 90
+        }),
+        imageminOptipng(),
+        imageminSvgo({
+          plugins: [{
+            removeViewBox: false
+          }]
+        })
+      ]))
+    .pipe(dest('dist/assets/img'))
+    .pipe(reload({
+      stream: true
+    }));
 }
 // değişiklikleri izleme
 function live() {
-    watch('src/scss/**/*', series(scss));
-    watch('src/js/*.js', series(js));
-    watch('src/js/single/*.js', series(singlejs));
-    watch('src/js/plugin/*.js', series(pluginjs));
-    watch('src/views/**/*.pug', series(pughtml));
-    watch('src/icon/*.svg', series(icon));
-    watch('src/img/sprite/*.png', series(sprite));
-    watch('src/img/*', series(imgmin));
-    watch('src/font/*', series(font));
+  watch('src/scss/**/*', series(scss));
+  watch('src/scss/**/*', series(scssAmp));
+  watch('src/js/*.js', series(js));
+  watch('src/js/single/*.js', series(singlejs));
+  watch('src/js/plugin/*.js', series(pluginjs));
+  watch('src/views/**/*.pug', series(pughtml));
+  watch('src/icon/*.svg', series(icon));
+  watch('src/img/sprite/*.png', series(sprite));
+  watch('src/img/*', series(imgmin));
+  watch('src/font/*', series(font));
 }
 
 exports.pughtml = pughtml;
 exports.sprite = sprite;
 exports.icon = icon;
 exports.scss = scss;
+exports.scssAmp = scssAmp;
 exports.browser = browser;
 exports.js = js;
 exports.singlejs = singlejs;
